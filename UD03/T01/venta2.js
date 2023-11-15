3d0bde700897353516f538f1458d6c275177e5ec
Daniel-Blas <a22carlosba@iessanclemente.net>
2023-11-15 13:01:07 +0100

    t01

---
+++ UD03/T01/venta2.js
@@ -0,0 +1,9 @@
+let url = document.getElementById("url");
+let protocolo = document.getElementById("protocolo");
+let navegador = document.getElementById("navegador");
+let java = document.getElementById("java");
+
+url.innerHTML= "URL: " +window.location;
+protocolo.innerHTML= "Protocolo: " +window.location.protocol;
+navegador.innerHTML= "Navegador: " +window.navigator.appCodeName;
+java.innerHTML= "Java dispoñible: " +(window.navigator.javaEnabled()?"Sí":"No");
