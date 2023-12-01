3d6f3025298b921a1670b44833b4c103098611b7
Daniel-Blas <a22carlosba@iessanclemente.net>
2023-12-01 14:04:16 +0100

    exercicio 1 e 2

---
+++ UD04/T01/arraysefuncions.js
@@ -0,0 +1,22 @@
+// Creación do array de paises
+let paises = ["España", "Portugal", "Andorra", "Francia", "Marruecos", "Reino Unido", "Irlanda", "Suiza", "Bélgica"];
+
+
+// Exercicio 1
+const numeroElementos = (arr) =>{
+    return arr.length;
+}
+
+document.getElementById("sol1").innerHTML = numeroElementos(paises);
+
+// Exercicio 2
+
+const amosarPaises = (arr) =>{
+    let paisesStr = "";
+    for (i in arr){
+        paisesStr += (arr[i] + ", ");
+    }
+    return paisesStr.slice(0, paisesStr.length -2);
+}
+
+document.getElementById("sol2").innerHTML = amosarPaises(paises);
