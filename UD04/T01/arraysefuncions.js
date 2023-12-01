601b70b9f2e40ad859b389e13fec254afbb26955
Daniel-Blas <a22carlosba@iessanclemente.net>
2023-12-01 14:26:31 +0100

    exercicio 4, 5 e correxido o 3

--- UD04/T01/arraysefuncions.js
+++ UD04/T01/arraysefuncions.js
@@ -23,7 +23,32 @@ document.getElementById("sol2").innerHTML = amosarPaises(paises);
 
 // Exercicio 3
 const sentidoInverso = (arr) =>{
-    return amosarPaises(arr.reverse());
+    let paisesInv = "";
+    for (let i = arr.length -1; i>=0; i--){
+        paisesInv += (arr[i] + ", ");
+    }
+    return paisesInv.slice(0, paisesInv.length -2);
 }
 
 document.getElementById("sol3").innerHTML = sentidoInverso(paises);
+
+// Exercicio 4
+const engadirComezo = (arr, pais) =>{
+    console.log(arr.unshift(pais));
+    return arr;
+}
+paises = engadirComezo(paises, "Nueva Zelanda");
+console.log(paises);
+
+document.getElementById("sol4").innerHTML = amosarPaises(paises);
+
+
+// Exercicio 5
+const engadirFinal = (arr, pais) =>{
+    console.log(arr.push(pais));
+    return arr;
+}
+paises = engadirFinal(paises, "Italia");
+console.log(paises);
+
+document.getElementById("sol5").innerHTML = amosarPaises(paises);
