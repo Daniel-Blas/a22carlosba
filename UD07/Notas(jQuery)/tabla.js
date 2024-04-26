18d9c8ceafac0a6b8e939e0f24bb302ad552f346
Daniel-Blas <a22carlosba@iessanclemente.net>
2024-04-26 12:01:56 +0200

    jQuery terminado

--- UD07/Notas(jQuery)/tabla.js
+++ UD07/Notas(jQuery)/tabla.js
@@ -3,23 +3,46 @@ $().ready(() => {
   $("#mitabla tr:nth-child(even)").addClass("colorido");
   // Ex 2: Calcular a media e a ponderación
   $("#calcular").click(() =>{
-    // engadir columna nota media (se non existe)
-    $("tr:first-child").append("<th>Nota Media</th>");
-    // calcular nota media
-    $("tr").each((index) =>{
-      console.log(index);
+    // Comprobar que non existan as columnas de nota media e ponderación
+    let existe = false;
+    $("tr:first-child").find("th").each((index, element) =>{
+      if ($(element).text() == "Nota Media"){
+        existe = true;
+      }
     })
-    // engadir nota media
-    // engadir columna ponderación
-    // calcular ponderación
-    // engadir ponderación
+    
+    if (!existe){
+      
+      // engadir columna nota media e ponderación
+      $("tr:first-child").append("<th>Nota Media</th>");
+      $("tr:first-child").append("<th>Ponderación</th>");
+      // calcular nota media e ponderación
+      notaMedia();
+    }
   }) 
-  });
-  
-  
-$("tr").append("<th>Hola</th>")
+});
 
+function notaMedia(){
+  // para cada fila calcular a nota media e a ponderación
+  $("tr").each((index, element) =>{
+    let suma = 0;
+    let divisor = 0;
+    if (index > 0){
+      $(element).find("td").each((index, nota) =>{
+        suma += parseFloat($(nota).text());
+        divisor++;
+      })
+      media = parseFloat(suma / divisor).toFixed(2);
+      pond = parseFloat(media *0.2).toFixed(2);
 
-//   Deberá tamén ter un botón que, cando se prema, calcule a nota media das tarefas e a súa
-// ponderación final (20% da nota na avaliación). Esas dúas últimas columnas poden xerarse en
-// tempo de execución ou telas creadas xa previamente no HTML, queda á túa escolla.
+      // engadir nota media e ponderación
+      $(element).append("<td>"+media+"</td>");
+      $(element).append("<td>"+pond+"</td>");
+      $(element).children().addClass((media>=5?"aprobado":"suspenso"))
+    }
+})
+}
+
+function colorAprobado(e){
+
+}
